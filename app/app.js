9574a50ce8b811c41d0653947681506868354922
Cheryl Collier <cherylanncollier@gmail.com>
2015-02-17 13:54:35 +0000

    added daft js

--- app/app.js
+++ app/app.js
@@ -12,25 +12,45 @@ xmlhttp.open("GET", url, true);
 xmlhttp.send();
 
 function myFunction(arr) {
-    var items = buildItems(arr);
-    document.querySelectorAll(".stuff")[0].innerHTML = items;
+    var elements = buildItems(arr),
+      i = 0,
+      container = document.querySelectorAll(".main.content")[0];
+    for(i = 0; i < elements.length; i++) {
+        container.appendChild(elements[i]);
+    }
 }
 
 function buildItems(items) {
     var i = 0,
-      str = '';
-    for(var i = 0; i < items.length; i++) {
-        str += '<div class="fp">';
-        str += '<img src="' + items[i].imageUrl + '" alt="' + items[i].title + '" class="fp-' + i + '">';
-        str += '<div class="info">';
-        str += '<h2>' + items[i].title + '</h2>';
-        str += '<hr>';
-        str += '<p>' + items[i].description + '</p>';
-        str += '<a href="/tutorials/' + items[i].id + '" title="' + items[i].title + '"></a>';
-        str += '</div>';
-        str += '</div>';
+      els = [];
+    
+    for(i = 0; i < items.length; i++) {
+        var obj = document.createElement("div");
+        obj.setAttribute("class", "fp");
+        var img = document.createElement("img");
+        img.setAttribute("src", items[i].imageUrl);
+        img.setAttribute("alt", items[i].title);
+        img.setAttribute("class", ("fp-" + i));
+        obj.appendChild(img);
+        var info = document.createElement("div");
+        info.setAttribute("class", "info");
+        var h2 = document.createElement("h2");
+        h2.innerHTML = items[i].title;
+        info.appendChild(h2);
+        var hr = document.createElement("hr");
+        info.appendChild(hr);
+        var p = document.createElement("p");
+        p.innerHTML = items[i].description;
+        info.appendChild(p);
+        var link = document.createElement("a");
+        link.setAttribute("href", ("/tutorials/" + items[i].id ));
+        link.setAttribute("title", items[i].title);
+        info.appendChild(link);
+        obj.appendChild(info);
+        
+        els[i] = obj;
     }
-    return(str);
+    return(els);
 }
 
 
